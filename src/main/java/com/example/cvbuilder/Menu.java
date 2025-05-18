58def8ebc72c9466481df3dba94f8fac5c1a31dd
qemashviliii <luka.q.luka@gmail.com>
2025-05-18 11:26:00 +0400

    Created Menu, Login, Register Pages and connected code to the database

---
+++ src/main/java/com/example/cvbuilder/Menu.java
@@ -0,0 +1,210 @@
+package com.example.cvbuilder;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.w3c.dom.Text;
+
+import java.sql.*;
+
+
+public class Menu extends Application {
+    private static String url = "jdbc:postgresql://localhost:5432/CVBuilderApplication";
+    private static String user = "postgres";
+    private static String password = "Grafikaferwera12.";
+    private static Connection connection;
+    private static Connection getConnection() throws SQLException {
+        if (connection == null) {
+            connection = DriverManager.getConnection(url, user, password);
+        }
+        return connection;
+    }
+
+
+
+
+    private Stage primaryStage;
+    private boolean isRegistered(String username,String email){
+        String checkQuery = "SELECT 1 FROM Accounts WHERE username = ? OR email = ? ";
+        try (Connection connection1 = getConnection();
+             PreparedStatement statement = getConnection().prepareStatement(checkQuery)) {
+            statement.setString(1, username);
+            statement.setString(2, email);
+
+            ResultSet rs = statement.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    @Override
+    public void start(Stage primaryStage) {
+
+        this.primaryStage=primaryStage;
+         ShowMenu();
+         this.primaryStage.show();
+
+    }
+    private void ShowMenu() {
+        Label titleLabel = new Label("Main Menu");
+        titleLabel.getStyleClass().add("label-title");
+
+        Button loginButton = new Button("Login");
+        Button registerButton = new Button("Register");
+
+        loginButton.getStyleClass().add("button");
+        registerButton.getStyleClass().add("button");
+
+        loginButton.setOnAction(e -> ShowLoginPage());
+        registerButton.setOnAction(e -> ShowRegisterPage());
+
+        VBox buttonBox = new VBox(10, loginButton, registerButton);
+        buttonBox.setAlignment(Pos.CENTER);
+
+        VBox form = new VBox(20, titleLabel, buttonBox);
+        form.setAlignment(Pos.CENTER);
+        form.getStyleClass().add("login-container");
+
+        StackPane root = new StackPane(form);
+        root.setPrefSize(400, 250);
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("menu.css").toExternalForm());
+
+        primaryStage.setScene(scene);
+    }
+
+    private void ShowLoginPage() {
+        Label titleLabel = new Label("Login");
+        titleLabel.getStyleClass().add("label-title");
+
+        TextField userField = new TextField();
+        userField.setPromptText("Username");
+        userField.getStyleClass().add("text-field");
+
+        TextField passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+        passwordField.getStyleClass().add("password-field");
+
+        Button loginButton = new Button("Login");
+        Button menuButton = new Button("Go Back");
+        loginButton.getStyleClass().add("button");
+        menuButton.getStyleClass().add("button");
+
+        HBox buttonBox = new HBox(10, loginButton, menuButton);
+        buttonBox.setAlignment(Pos.CENTER);
+
+        VBox form = new VBox(15, titleLabel, userField, passwordField, buttonBox);
+        form.setAlignment(Pos.CENTER);
+        form.getStyleClass().add("login-container");
+
+        StackPane root = new StackPane(form);
+        root.setPrefSize(400, 300);
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+        menuButton.setOnAction(e -> ShowMenu());
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void ShowRegisterPage() {
+        Label titleLabel = new Label("Register Account");
+        titleLabel.getStyleClass().add("label-title");
+
+        Label messageLabel = new Label();
+        messageLabel.getStyleClass().add("label-error");
+
+        TextField usernameField = new TextField();
+        usernameField.setPromptText("Username");
+        usernameField.getStyleClass().add("text-field");
+
+        TextField emailField = new TextField();
+        emailField.setPromptText("Email");
+        emailField.getStyleClass().add("text-field");
+
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+        passwordField.getStyleClass().add("password-field");
+
+        Button registerButton = new Button("Register");
+        Button menuButton = new Button("Back to Menu");
+
+        registerButton.getStyleClass().add("button");
+        menuButton.getStyleClass().add("button");
+
+        HBox buttonBox = new HBox(10, registerButton, menuButton);
+        buttonBox.setAlignment(Pos.CENTER);
+
+        VBox form = new VBox(15, titleLabel, usernameField, emailField, passwordField, buttonBox,messageLabel);
+        form.setAlignment(Pos.CENTER);
+        form.getStyleClass().add("login-container");
+
+        StackPane root = new StackPane(form);
+        root.setPrefSize(400, 350);
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+        menuButton.setOnAction(e -> ShowMenu());
+
+
+        registerButton.setOnAction(e->{
+            String username = usernameField.getText();
+            String email = emailField.getText();
+            String password = passwordField.getText();
+
+            if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                messageLabel.setText("Please fill fields");
+                return;
+            }
+
+            if (isRegistered(username, email)) {
+                messageLabel.setText("This User or Email is already used.");
+                return;
+            }
+
+
+
+            String insertQuery = "INSERT INTO Accounts (username, email, password) VALUES (?, ?, ?)";
+            try (PreparedStatement statement = getConnection().prepareStatement(insertQuery)){
+                statement.setString(1,username);
+                statement.setString(2,email);
+                statement.setString(3,password);
+                statement.executeUpdate();
+                messageLabel.setText("Added successfully");
+            }catch (SQLException exception){
+                exception.printStackTrace();
+                messageLabel.setText("Registration went wrong!" + exception.getMessage());
+            }
+        });
+
+
+
+
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.cvbuilder {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.sql;
 
     opens com.example.cvbuilder to javafx.fxml;
     exports com.example.cvbuilder;
